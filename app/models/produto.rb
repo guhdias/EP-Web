f29fbd5cd2032b08bcd89779edc779daaeb8a856
guhdias <guhdias@me.com>
2015-01-05 03:18:46 -0200

    Validações

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,3 +1,9 @@
 class Produto < ActiveRecord::Base
 	belongs_to :tipo
+	
+	validates_presence_of :tipo_id, message: "Escolha um Tipo"
+	validates_presence_of :cbarras, message: "Digite o codigo de barras"
+	validates_presence_of :descricao, message: "Digite uma descrição"
+	validates_presence_of :valor, message: "Digite um valor"
+	validates_presence_of :saldo, message: "Informe o saldo do produto"
 end
